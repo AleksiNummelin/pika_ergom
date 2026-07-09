3d378e26849b73e6ddb55316982d0688d041bf3c
Andrew Gerard Twelves <51830799+atwelves@users.noreply.github.com>
2026-07-09 18:30:36 +0300

    Update comp_cmems.py

--- code/comp_cmems.py
+++ code/comp_cmems.py
@@ -3,7 +3,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-run_id =                                                                                   ['ctrl'],# names of pika-ergom iterations     
+run_id =                                                                                   ['ctrl']#, names of pika-ergom iterations     
                                                                                            # 'slow-sink']
 dpt    =                                                                                    20  # depth
 units  =                                                                                  'mmol m⁻³' # units
