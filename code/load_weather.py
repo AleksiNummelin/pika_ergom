c771a0c570209eae498815812075f7c4a6f75879
Andrew Gerard Twelves <51830799+atwelves@users.noreply.github.com>
2026-07-10 13:02:24 +0300

    Update comments on load_weather.py

--- code/load_weather.py
+++ code/load_weather.py
@@ -2,14 +2,11 @@ import datetime as dt
 import numpy as np
 
 def load_weather(input_scalar, forcing_matrix, counter):
- 
-#  print("model day","forcing day")  
-#  print(model_day,forcing_day)
 
-  final_scalar = forcing_matrix.iloc[counter].values 
+  final_scalar = forcing_matrix.iloc[counter].values #assumes that forcing and model both have hourly timestep... 
   
   if(final_scalar=="-"):
-      # missing values
+      # deal with missing values by using last valid measurement
       final_scalar = input_scalar
   
   final_scalar = final_scalar.astype(float)
