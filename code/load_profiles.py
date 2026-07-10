69c3df18e6196770dda4ce57d4b6b40a648a3ede
Andrew Gerard Twelves <51830799+atwelves@users.noreply.github.com>
2026-07-10 13:00:18 +0300

    Update comments on load_profiles.py

--- code/load_profiles.py
+++ code/load_profiles.py
@@ -2,16 +2,13 @@ import datetime as dt
 import numpy as np
 
 def load_profiles(forcing_matrix,model_day,forcing_day,forcing_index,kmax):
- 
-#  print("model day","forcing day")  
-#  print(model_day,forcing_day)
 
-  # check for new day
+  # check to see if we are on a new day
   if(model_day>forcing_day+1):  
       forcing_index = forcing_index + 1
       forcing_day   = forcing_day + 1
 
-  # load the forcing
+  # load the ocean profiles
   final_vector = forcing_matrix[forcing_index,:kmax]
   final_vector = np.squeeze(final_vector)
   return(final_vector,forcing_day,forcing_index)
