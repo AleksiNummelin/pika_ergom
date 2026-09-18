1ca35b0e583269b0fb1bdfc8ae1aa2d0a09b49ab
Andrew Gerard Twelves <51830799+atwelves@users.noreply.github.com>
2026-09-18 13:47:06 +0300

    Tidy up plotting script

--- code/comp_cmems.py
+++ code/comp_cmems.py
@@ -3,7 +3,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-run_id =                                                                                   ['ctrl']#, names of pika-ergom iterations     
+colorbrewer = [(166/255, 97/255, 26/255),
+               (223/255,194/255,125/255),
+               (128/255,205/255,193/255),
+               (  1/255,133/255,113/255)]
+
+run_id =                                                                                   ['cmems_iow','cmems_fmi','IU7_iow','IU7_fmi']#, names of pika-ergom iterations     
 dpt    =                                                                                        0  # depth
 units  =                                                                                'microgram / L' # units
 var    =                                                                                     'chl' # variable
@@ -14,9 +19,10 @@ ds    = xr.open_dataset('init/cmems_station_Utö_bgc_{}.nc'.format(year)).sel(de
 cmems = np.squeeze(ds.values)
 times = ds.time.values
 
-plt.figure()
-
+plt.figure(figsize=(20,10))
+i=-1
 for name in run_id:
+    i=i+1
     # read in an iteration of pika-ERGOM, and find the closest depth to that requested
     ds         = xr.open_dataarray('{}_{}.nc'.format(name,var)).sel(depth=dpt,method='nearest')
     pika       = np.squeeze(ds.values)
@@ -25,20 +31,25 @@ for name in run_id:
     # read time axis
     pika_time  = ds.time.values
     # plot iteration as time series
-    plt.plot(pika_time,pika,label='pika-ergom_{}'.format(name))
+    plt.plot(pika_time,pika,label='pika-ergom_{}'.format(name),linewidth=5,color=colorbrewer[i])
 
 # flow cytometry data
 #infile = ''
 #ds = xr.open_dataset('{}/IFCB_flow_{}.nc'.format(infile,year))
-#print(ds)
+print(ds)
 #flow_chl = ds.Chla_new_calc.values
 #flow_times = ds.time.values
 #plt.plot(flow_times,flow_chl,label='flow cytometry')
 
 # plot reanalysis as reference
-plt.plot(times,cmems,label='cmems-reanalysis')
-plt.ylabel('{}, {}'.format(var,units))
-plt.title('Depth = {0:.1f}m'.format(real_depth))
+plt.plot(times,cmems,label='BALMFC reanalysis',linewidth=5,linestyle='--',color='k')
+plt.yticks(fontsize=20)
+plt.ylabel('{}, {}'.format(var,units),fontsize=30)
+plt.title('Depth = {0:.1f}m'.format(real_depth),fontsize=30)
+plt.xlim(times[0],times[-1])
+times = np.unique(times.astype('datetime64[M]'))
+print(times)
+plt.xticks(times,times.astype('datetime64[M]'),fontsize=15)
 plt.grid()
-plt.legend()
+plt.legend(fontsize=20)
 plt.savefig('comp_{}_{}.png'.format(var,dpt))
